e791cbf7b157946a3988671dc11a64ddd5ec4c81
MathCT <mathieu.chatrain@gmail.com>
2025-10-23 09:12:31 +0200

    commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,7 @@ const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'dev_battle_arena',
+  database: process.env.DB_NAME || 'dev_battle_arena_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
